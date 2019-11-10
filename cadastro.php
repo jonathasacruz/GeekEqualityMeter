d945de0f406ec7dbea6b936b398bdd8e2fb2d4e8
Jônathas Cruz <jonathas.acruz@users.noreply.github.com>
2019-11-10 15:53:21 -0300

    Criada integração com backend: página de cadastro insere novos usuários em banco de dados MySQL
    Falta página de login e CSS

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+        $nome ='nome_cad';
+        $login = 'login';
+        $email = 'email_cad';
+        $senha = 'senha_cad';
+
+
+
+        $_GET['nome'] = $nome;
+        $_GET['login'] = $login;
+        $_GET['email'] = $email;
+        $_GET['senha'] = $senha;
+
+?>
+
+
+define ('HOST', 'localhost'); //Host do servidor
+define('USER', 'root'); Usuário do phpmyadmin
+define
