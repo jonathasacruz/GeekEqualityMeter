f122ad3192b2759e2e18af134e6bb059f5dc5d3c
Jônathas Cruz <jonathas.acruz@users.noreply.github.com>
2019-11-19 01:44:07 -0300

    Recriado banco de dados

--- control/conexao.php
+++ control/conexao.php
@@ -13,26 +13,12 @@ if (!$conn) {
 }
 
 $nome_cad = $_POST['nome_cad'];
-$login_cad = $_POST['login_cad'];
 $email_cad = $_POST['email_cad'];
+$login_cad = $_POST['login_cad'];
 $senha_cad = $_POST['senha_cad'];
 
-/*
 
-$nome_cad = mysqli_escape_string($conn, 'nome_cad');
-$login_cad = mysqli_escape_string($conn, 'login_cad');
-$email_cad = mysqli_escape_string($conn, 'email_cad');
-$senha_cad= mysqli_escape_string($conn, 'senha_cad');
-*/
-
-/*
- $nome_cad = 'nome_cad';
- $login_cad = 'login_cad';
- $email_cad = 'email_cad';
- $senha_cad = 'senha_cad';
-*/
-
-$sql = "INSERT INTO usuarios (nome, login, senha, email) VALUES ('$nome_cad','$login_cad', '$senha_cad', '$email_cad')";
+$sql = "INSERT INTO usuarios (nome, email, login, senha) VALUES ('$nome_cad','$email_cad', '$login_cad', '$senha_cad')";
 
 //mysql_query($sql,$conn);
 
